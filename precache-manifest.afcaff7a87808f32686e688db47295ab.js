d1d6792427703cf76892bd833e5b957111937f43
JustinNgWeiShung <justin.shung@gmail.com>
2020-05-17 15:31:55 +0800

    initial commit

---
+++ precache-manifest.afcaff7a87808f32686e688db47295ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800f268e444b62d93c539e81b2ac6508",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d5d9e780d66af37fd65",
+    "url": "/static/css/main.85822eb3.chunk.css"
+  },
+  {
+    "revision": "ab22026bb4c3f76e3bf0",
+    "url": "/static/js/2.eb0d9077.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eb0d9077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5d9e780d66af37fd65",
+    "url": "/static/js/main.372e173b.chunk.js"
+  },
+  {
+    "revision": "c3ee397664833e4fb798",
+    "url": "/static/js/runtime-main.802a2b98.js"
+  }
+]);

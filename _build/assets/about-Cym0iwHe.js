d0928c2ca7d8b09c2658959a4d143c6fb730d353
Justin Ng Wei Shung <justin.shung@gmail.com>
2025-01-21 20:42:10 +0800

    Auto-generated commit

---
+++ _build/assets/about-Cym0iwHe.js
@@ -0,0 +1,1 @@
+import{t}from"./web-Cl559qu1.js";var e=t("<div><h1 class=text-3xl>About me");function o(){return e()}export{o as default};
---
+++ _build/assets/projects-zzHpcPJo.js
@@ -0,0 +1,1 @@
+import{j as e,t as a}from"./web-Cl559qu1.js";var n=a("<div>");function i(r){return(()=>{var t=n();return e(t,()=>r.children),t})()}export{i as default};
